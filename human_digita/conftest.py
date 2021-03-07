18b13f7226158f958ed019735b71908c5e9d3b83
boan-anbo <gazagoal@gmail.com>
2021-03-07 14:40:35 +0100

    first commit

---
+++ human_digita/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from human_digita.users.models import User
+from human_digita.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
